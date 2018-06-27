2610dfe419428084dff36e575cb3147564a6a9bb
nkiyo <nkiyo@hoge.com>
2018-06-27 09:17:56 +0900

    first commit

---
+++ format_code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+astyle \
+    --style=attach `#hoge` \
+    -s4  `#hoge` \
+    --attach-namespaces  `#hoge` \
+    --pad-oper  `#hoge` \
+    --pad-comma  `#hoge` \
+    --align-pointer=name  `#hoge` \
+    --align-reference=type  `#hoge` \
+    --break-one-line-headers  `#hoge` \
+    --convert-tabs  `#hoge` \
+    main.cc
---
+++ main.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+namespace unk {
+
+class DummyClass {
+public:
+    int func(int x, int y);
+    int func2(const int& x, int *y);
+private:
+    int x_;
+    double y_;
+};
+
+/**
+ * サンプル関数
+ * @param x hoge
+ * @param y hage
+ * @return hhhh
+ */
+void DummyClass::func(int x,
+                      int y) {
+    x += 2;
+    y   =   0 + y;
+    int z = x + y; /* てすと */
+    return z; // てすと
+}
+
+int main() {
+    DummyClass *dc = new DummyClass();
+    dc->func(1, 1);
+    printf("hello\n");
+}
+
+}
+int DummyClass::func2(const int& x, int *y) {
+    if(x > 1)
+        return (x + y);
+    printf("hoge\n");
+    if(x <= 0) {
+        return 0;
+    }
+}
+
